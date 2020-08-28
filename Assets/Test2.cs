e27b842f85912ad0f5934119fbbb223f70258cb5
TAK334 <venomsnake5555@gmail.com>
2020-08-28 20:52:30 +0900

    2回目のリポジトリ作成
    課題提出に必要なものを作成しました。

---
+++ Assets/Test2.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Test2: MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // 5回処理を繰り返す
+        int a[5];
+        {
+            Debug.Log(i);
+        }
+    }
+{
+   
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
+public class  : MonoBehaviour
+{
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    // 5回処理を繰り返す
+    for (int i = 0; i < 5; i++) }
+        {
+            Debug.Log(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+}
+}
+
+
+
+   public class  : MonoBehaviour
+{
+}
+
+// Use this for initialization
+void Start()
+{
+    // 5回処理を繰り返す
+    for (i = 4; i >= 0; i--)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            Debug.Log(i);
+    }
+}
+
+// Update is called once per frame
+void Update()
+{
+
+}
+}
